df3dc416f092b3a603a264c3cb11599817d25969
Dennis Fashimpaur <Fashimpaur@users.noreply.github.com>
2025-10-06 14:48:42 -0500

    adds gcd_lcm problem description and Pipfile + Pipfile.lock

---
+++ sorting_greedy_math/gcd_lcm.py
@@ -0,0 +1,26 @@
+"""
+Problem: Find the GCD and LCM of Two Numbers
+
+Write a function:
+
+def solution(A: int, B: int) -> tuple[int, int]:
+    pass
+
+that, given two positive integers A and B, returns a tuple (G, L) where:
+
+- G is the greatest common divisor (GCD) of A and B.
+- L is the least common multiple (LCM) of A and B.
+
+Example:
+
+solution(15, 20)  # returns (5, 60)
+solution(7, 5)    # returns (1, 35)
+solution(21, 6)   # returns (3, 42)
+solution(13, 13)  # returns (13, 13)
+
+Assumptions:
+
+- A and B are integers within the range [1..2,147,483,647].
+- The function should return the correct answer efficiently, even for large values of A and B.
+"""
+
